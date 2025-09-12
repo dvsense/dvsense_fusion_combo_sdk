f4fc960ccd2b38cc09f0409b12e82529cec6fb08
yuekang.wang@dvsense.com <yuekang.wang@dvsense.com>
2025-09-12 11:44:21 +0800

    FIX:优化docs中类的说明文档

--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['start_0',['start',['../class_dvs_rgb_fusion_camera.html#ac92d3b835636641550b12b0f93daf21d',1,'DvsRgbFusionCamera']]],
-  ['startrecording_1',['startRecording',['../class_dvs_rgb_fusion_camera.html#ac7d9d6115f0289acf02d228d0ba70d5e',1,'DvsRgbFusionCamera']]],
-  ['stop_2',['stop',['../class_dvs_rgb_fusion_camera.html#ae51416af540512a3ba419ad44754360e',1,'DvsRgbFusionCamera']]],
-  ['stoprecording_3',['stopRecording',['../class_dvs_rgb_fusion_camera.html#a38a9216d672ae71e7823ca1f3cc5c371',1,'DvsRgbFusionCamera']]]
+  ['opencamera_0',['openCamera',['../class_dvs_rgb_fusion_camera.html#a9327f58685a98fff1cc9dea230af39df',1,'DvsRgbFusionCamera']]]
 ];
