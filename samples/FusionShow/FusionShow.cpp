4fcaf75bb0336c6b2df6ea45783a50c1a9e39e43
yuekang.wang@dvsense.com <yuekang.wang@dvsense.com>
2025-12-04 18:02:05 +0800

    FEAT: add rgb frame move

--- samples/FusionShow/FusionShow.cpp
+++ samples/FusionShow/FusionShow.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[])
 	    data_save_path = argv[1];	
 	}
 
-	bool is_calibration_active = true;
+	bool is_calibration_active = false;
 
 	std::unique_ptr<CalibrateThroughFile> calibrator = std::make_unique<CalibrateThroughFile>("./calibration_result.json");
 	cv::Mat H = calibrator->getApsToDvsHomographyMatrix(1000);
@@ -32,7 +32,8 @@ int main(int argc, char* argv[])
 	std::queue<cv::Mat> image_display_queue;
 	std::mutex display_image_mutex;
 
-	std::unique_ptr<DvsRgbFusionCamera<DahengCamera>> fusionCamera = std::make_unique<DvsRgbFusionCamera<DahengCamera>>(30);
+	std::unique_ptr<DvsRgbFusionCamera<HikCamera>> fusionCamera = std::make_unique<DvsRgbFusionCamera<HikCamera>>(30);
+	//std::unique_ptr<DvsRgbFusionCamera<DahengCamera>> fusionCamera = std::make_unique<DvsRgbFusionCamera<DahengCamera>>(30);
 
 	std::vector<dvsense::CameraDescription> dvs_serials;
 	std::vector<std::string> rgb_serials;
@@ -77,8 +78,10 @@ int main(int argc, char* argv[])
 		}
 	);
 
+	int offset_x = 0;
+	int offset_y = 0;
 	fusionCamera->addApsFrameCallback(
-		[&is_calibration_active, &calibrator, &H, &image_display_queue, &new_dvs_frame, &dvs_width, &dvs_height, &dvs_frame_mutex, &display_image_mutex](const dvsense::ApsFrame& rgbframe)
+		[&offset_x, &offset_y, &is_calibration_active, &calibrator, &H, &image_display_queue, &new_dvs_frame, &dvs_width, &dvs_height, &dvs_frame_mutex, &display_image_mutex](const dvsense::ApsFrame& rgbframe)
 		{
 			if (rgbframe.getDataSize() != 0 && image_display_queue.empty())
 			{
@@ -93,21 +96,26 @@ int main(int argc, char* argv[])
 				if (!is_calibration_active)
 				{
 
-					static int new_dvs_width = dvs_width * 1.215;
-					static int new_dvs_height = dvs_height * 1.215;
+					static int new_dvs_width = dvs_width * (4.86 / 2.5);
+					static int new_dvs_height = dvs_height * (4.86 / 2.5);
 
 					int y_start = (aps_height - new_dvs_height) / 2;
 					int x_start = (aps_width - new_dvs_width) / 2;
 					aps_resize = reconstructed_image(cv::Rect(x_start, y_start, new_dvs_width, new_dvs_height));
 					cv::resize(aps_resize, aps_resize, cv::Size(dvs_width, dvs_height), cv::INTER_AREA);
+
+					cv::Mat translation_matrix = (cv::Mat_<double>(2, 3) << 1, 0, offset_x, 0, 1, offset_y);
+					cv::Mat move_result;
+					cv::warpAffine(aps_resize, move_result, translation_matrix, aps_resize.size(),
+						cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
 					{
 						std::unique_lock<std::mutex> lock(dvs_frame_mutex);
-						aps_resize.setTo(cv::Scalar(0, 0, 0), new_dvs_frame);
-						aps_resize = aps_resize + new_dvs_frame;
+						move_result.setTo(cv::Scalar(0, 0, 0), new_dvs_frame);
+						move_result = move_result + new_dvs_frame;
 						new_dvs_frame.setTo(cv::Scalar(0, 0, 0));
 					}
 					std::unique_lock<std::mutex> lock(display_image_mutex);
-					image_display_queue.emplace(aps_resize);
+					image_display_queue.emplace(move_result);
 				}
 				else
 				{
@@ -166,6 +174,19 @@ int main(int argc, char* argv[])
 				fusionCamera->stopRecording();
 			}
 			is_recording = !is_recording;
+		}
+
+		if ((key & 0xFF) == '8') {  // 上键
+			offset_y--;
+		}
+		else if ((key & 0xFF) == '2') {  // 下键
+			offset_y++;
+		}
+		else if ((key & 0xFF) == '4') {  // 左键
+			offset_x--;
+		}
+		else if ((key & 0xFF) == '6') {  // 右键
+			offset_x++;
 		}
 	}
 	if (is_recording)
